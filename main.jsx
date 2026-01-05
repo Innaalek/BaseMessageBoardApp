5704c5d0c14a4173fc64f0189c6b7027594d71f5
Innaalek <pavlovainna529@gmail.com>
2026-01-05 21:45:46 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MessageBoard from "./components/MessageBoard";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("react-root")).render(
   <React.StrictMode>
     <MessageBoard />
   </React.StrictMode>
