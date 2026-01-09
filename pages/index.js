3bd23b23295e446d7d8c19c6c575b6ec1528d72f
Innaalek <pavlovainna529@gmail.com>
2026-01-09 23:47:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Head from 'next/head';
 import MessageBoard from "../components/MessageBoard";
 
 export default function Home() {
-  return <MessageBoard />;
+  return (
+    <>
+      <Head>
+        <title>Base Message Board</title>
+        <meta name="description" content="Decentralized message board on Base Network" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://base.org/document/og-image.png" />
+        <meta property="og:title" content="Base Message Board" />
+        <meta property="og:description" content="Leave your mark on the Base blockchain forever." />
+      </Head>
+      <MessageBoard />
+    </>
+  );
 }
